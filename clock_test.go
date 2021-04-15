18d802b53eaf880d1afa8cc218b88bada56e5e27
Tran Hau <ngtranhau@gmail.com>
2021-04-15 04:50:10 +0000

    test: unittest for FormatDateTime

--- clock_test.go
+++ clock_test.go
@@ -32,3 +32,29 @@ func TestFormatDate(t *testing.T) {
 		})
 	}
 }
+
+func TestFormatDateTime(t *testing.T) {
+	tests := []struct {
+		name string
+		when time.Time
+		want string
+	}{
+		{
+			name: "2020-04-01 23:59:0",
+			when: time.Date(2020, 4, 1, 23, 59, 0, 0, time.Local),
+			want: "2020-04-01 23:59:00",
+		},
+		{
+			name: "2020-01-18 0:12:24",
+			when: time.Date(2020, 1, 18, 0, 12, 24, 0, time.Local),
+			want: "2020-01-18 00:12:24",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := FormatDateTime(tc.when)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
